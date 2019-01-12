9bbd905b92914868ed1baa1894333db3c57f5ed3
unknown <bokistef96@yahoo.com>
2019-01-12 18:07:14 +0100

    initial configuration

--- DVSleague/DVSleague/App_Start/RouteConfig.cs
+++ DVSleague/DVSleague/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace DVSleague
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
