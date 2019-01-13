4477fdd8bdfbb056668a67db255f9f619915fa7f
Petar Vasiljevic <petar.vasiljevic@elfak.rs>
2019-01-13 16:19:19 +0100

    Players and Teams views

---
+++ DVSleague/DVSleague/Controllers/TeamController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DVSleague.Models;
+using DVSleague.Repository3;
+
+namespace DVSleague.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("team", Name = "team_details")]
+        public ActionResult TeamDetails() //     view/home/Team/PlayerDetails
+        {
+            TeamRepository teamRepository = new TeamRepository();
+
+            Team team = new Team();
+
+            team=teamRepository.getTeamById(2);
+
+
+            ViewBag.Message = "Your application description page." + "dsddsd";
+
+            return View(team);
+        }
+    }
+}
