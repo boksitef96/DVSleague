b2367118c0a2b849bcf9fb7493e61cde9776f380
unknown <bokistef96@yahoo.com>
2019-01-14 19:02:47 +0100

    fix

--- DVSleague/DVSleague/Controllers/HomeController.cs
+++ DVSleague/DVSleague/Controllers/HomeController.cs
@@ -10,6 +10,11 @@ namespace DVSleague.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
